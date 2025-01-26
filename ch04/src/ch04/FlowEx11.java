f90d98eeb132acb3f4ab2321f07435cf14b3c97e
xyzli <kyu_107@naver.com>
2025-01-26 15:56:02 +0900

    switch문

---
+++ ch04/src/ch04/FlowEx11.java
@@ -0,0 +1,35 @@
+package ch04;
+
+import java.util.Scanner;
+
+public class FlowEx11 {
+
+	public static void main(String[] args) {
+		System.out.println("당신의 주민번호를 입력하세요.(011231-1111222)>");
+		
+		Scanner scanner = new Scanner(System.in);
+		String regNo = scanner.nextLine();
+		char gender = regNo.charAt(7); //입력 받은 7번째 문자를 gender에 저장
+		
+		switch(gender) {
+			case '1': case'3':
+				switch(gender) {
+					case '1':
+						System.out.println("2000년 이전에 출생한 남자입니다.");
+					case '3':
+						System.out.println("2000냔 이전에 출생한 여자입니다.");
+				}
+				break;				//이 break문을 빼먹지 않도록 주의
+			case '2': case '4':
+				switch(gender) {
+				case '2':
+					System.out.println("2000년 이후에 출생한 남자입니다.");
+				case '4':
+					System.out.println("2000년 이후에 출생한 여자입니다.");
+				}
+				break;
+				default:
+					System.out.println("유효하지 않은 주민등록번호입니다.");
+		}
+	}
+}
